e9f7776287a1930419417cd93b07c81b372f424c
LEARN Academy <sdunlap102326@gmail.com>
2023-05-04 13:02:21 -0700

    Added a bunch of styling but the homepage is not routing to HOME it is routing to localbranch/3000

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,13 @@
-
-
+import logo from './logo.png';
 
 const Home = () => {
+  return (
+    <div className="home-center">
+      <img src={logo} alt="Dog Tinder Logo" className="logo" />
+      <h1>Welcome To Dog Tinder!</h1>
+      <h2>It's Like Tinder, but for Dogs</h2>
+    </div>
+  );
+};
 
-    return (
-        <div className="home-center">
-            <h1>Welcome To Dog Tinder!</h1>
-            <br/>
-            <h2>It's Like Tinder, but for Dogs</h2>
-            {/* <img src="" */}
-            
-        </div>
-    )
-}
-
-export default Home
+export default Home;
